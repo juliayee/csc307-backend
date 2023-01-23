c9a00377a43009aad71a112f6575d0c2014e9bec
juliayee <jye13@calpoly.edu>
2023-01-23 00:21:21 -0800

    with cors

--- backend.js
+++ backend.js
@@ -2,7 +2,9 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 
 app.listen(port, () => {
